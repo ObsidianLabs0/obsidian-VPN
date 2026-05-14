2b91eca3b492131a2c60f1bd1b64b2f1f7e5ec74
ObsidianLabs0 <gavinsonnier42@gmail.com>
2026-05-14 14:59:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-import { defineConfig } from '@tanstack/start/config' // Recommended for TanStack Start
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-  vite: {
-    plugins: [react()], // Essential to make React work
-    server: {
-      host: "0.0.0.0",
-      port: 5000,
-      strictPort: true,
-      allowedHosts: "all", // Or a specific array of hosts
-    },
-  },
-});
+  plugins: [react()],
+})
